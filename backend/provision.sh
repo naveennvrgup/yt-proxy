9be611fbfa5880a4fd7c2e497700703ab898c0fc
naveennvrgup <naveennvrgup@gmail.com>
2020-12-29 09:46:17 +0530

    fix: provision script

--- backend/provision.sh
+++ backend/provision.sh
@@ -1,6 +1,6 @@
 virtualenv env -p=python3 
-pip install -r requirements.txt
 source env/bin/activate
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py shell < load-database.py
 python manage.py runserver 0.0.0.0:5556
